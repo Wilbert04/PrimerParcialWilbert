940417e64d1d06e619a5a1a88ee14321c4780db0
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2020-10-07 06:32:27 -0500

    Clase productos creada

---
+++ PrimerParcialWilbert-Ap2/Models/Productos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcialWilbert_Ap2.Models
+{
+    public class Productos
+    {
+        [Key]
+        [Range(0,1000000,ErrorMessage ="ProductoId incorrecto!!!, El limite permitido es de 0 a 1000000")]
+        public int ProductoId { get; set; }
+
+        [Required (ErrorMessage ="Este campo es obligatorio")]
+        public string Descripcion { get; set; }
+
+        [Range(minimum:1, maximum:200, ErrorMessage=" El maximo de existencia es de 200")]
+        public int Existencia { get; set; }
+
+        [Required (ErrorMessage = "El monto es obligatorio")]
+        public decimal Costo { get; set; }
+        public decimal ValorInventario { get; set; }
+
+    }
+}
